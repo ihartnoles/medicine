05147227b0dce1aea0006d5e8a6490607d990dfd
Isaac Hartstein <ihartstein@ubuntu.(none)>
2013-12-20 14:53:39 -0500

    display of usertype on user list/usertype drop-down on edit user screen

--- app/models/permissiongroup.rb
+++ app/models/permissiongroup.rb
@@ -2,5 +2,6 @@ class Permissiongroup < ActiveRecord::Base
   attr_accessible :groupname, :description, :bits
   has_many	:userpermissions
   has_many	:affiliatepermissions
+  has_many	:users
   validates :groupname, :description, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
   attr_accessible :firstname, :lastname, :phone, :username, :usertype_id
   has_many	:userpermissions
+  has_many	:permissiongroups
   validates :firstname, :lastname, :phone, :username, :usertype_id, :presence => true
+
+
+  def groupname(id)
+  	name = Permissiongroup.find(id).groupname
+
+  	return name
+  end
+
 end
