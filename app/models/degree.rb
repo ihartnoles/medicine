9479de686bcd2de463ec064979200708b0def864
IH <ihartnoles@gmail.com>
2014-01-17 12:43:55 -0500

    Degree CRUD - in progress

--- app/models/degree.rb
+++ app/models/degree.rb
@@ -1,7 +1,8 @@
 class Degree < ActiveRecord::Base
   #has_and_belongs_to_many :institutions
-  attr_accessible :affiliate_id, :degreename, :institution_id, :year
+  attr_accessible :affiliate_id, :degreename, :institution_id, :year, :city, :state, :country
 
   validates :year, :presence => true
   validates :institution_id, :presence => true
+  validates :state, :presence => true
 end
