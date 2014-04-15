a4f802e5706719f6efdeda2f172f1fe623ff38f9
Isaac Hartstein <ihartstein@ubuntu.(none)>
2014-04-15 11:19:51 -0400

    Banner status column in affiliate listing

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -8,5 +8,15 @@ class Banner < ActiveRecord::Base
   def self.get_objects
     self.find_by_sql("select * from FAUMGR.AWP_COM_ALL_EMPS order by last_name asc")
   end
+
+  def self.find_by_lastname(lastname)
+  	 tmp = self.find_by_sql(["select * from FAUMGR.AWP_COM_ALL_EMPS  where last_name = :last ", {:last => lastname }])
+
+  	if tmp.count > 0
+  		return true
+  	else 
+  		return false
+  	end 
+  end
   
 end
