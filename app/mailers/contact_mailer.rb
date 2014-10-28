8b46af5bc9a6512529fdf188522d06a4b60ab75d
Isaac Hartstein <ihartnoles@gmail.com>
2014-10-28 09:09:40 -0400

    Add medsearchhelp@fau.edu to contact form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -8,7 +8,7 @@ class ContactMailer < ActionMailer::Base
   	
  	
 
-    mail(:from => "#{@username}@fau.edu", :to => "eneedelm@fau.edu", :subject => "CoM Faculty Database - #{@subject}")
+    mail(:from => "#{@username}@fau.edu", :to => "medsearchhelp@fau.edu", :subject => "CoM Faculty Database - #{@subject}")
    end
 
 end
